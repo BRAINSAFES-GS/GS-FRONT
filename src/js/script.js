ec335c3f9e345f47b613f03919e52f1f25a38254
Laboratório FIAP <enricobborges06@gmail.com>
2025-05-27 10:27:26 -0300

    criando as funcoes js e estilizando

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+var imgAtual ="./src/assets/fundo3.jpg";
+var imgAnterior = "./src/assets/fundo1.jpg";
+
+function trocar(){
+    document.getElementById("fundo1").src =imgAtual;
+    let aux =imgAtual;
+    imgAtual=imgAnterior
+    imgAnterior=aux
+}var imgAtual = "./src/assets/fundo3.jpg";
+var imgAnterior = "./src/assets/fundo1.jpg";
+
+function trocar(){
+    document.getElementById("fundo1").src =imgAtual;
+    let aux =imgAtual;
+    imgAtual=imgAnterior
+    imgAnterior=aux}
