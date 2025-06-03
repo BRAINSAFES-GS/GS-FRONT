780069a273818b98b8cf57c7406729d5fcb8f710
enrico <enricobborges06@gmail.com>
2025-06-03 18:48:50 -0300

    estilizando e responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,38 @@
-var imgAtual ="./src/assets/fundo3.jpg";
-var imgAnterior = "./src/assets/fundo1.jpg";
+ const imagens = [
+    "./src/assets/fundo1.jpg",
+    "./src/assets/fundo3.jpg",
+    
+  ];
 
-function trocar(){
-    document.getElementById("fundo1").src =imgAtual;
-    let aux =imgAtual;
-    imgAtual=imgAnterior
-    imgAnterior=aux
-}var imgAtual = "./src/assets/fundo3.jpg";
-var imgAnterior = "./src/assets/fundo1.jpg";
+  let indiceAtual = 0;
+  const imgElemento = document.getElementById("fundo1");
+  const pontos = document.querySelectorAll('.ponto');
 
-function trocar(){
-    document.getElementById("fundo1").src =imgAtual;
-    let aux =imgAtual;
-    imgAtual=imgAnterior
-    imgAnterior=aux}
+  function atualizarPontos() {
+    pontos.forEach((ponto, index) => {
+      ponto.classList.toggle('ativo', index === indiceAtual);
+    });
+  }
+
+  function trocarImagem() {
+    imgElemento.style.opacity = 0;
+
+    setTimeout(() => {
+      indiceAtual = (indiceAtual + 1) % imagens.length;
+      imgElemento.src = imagens[indiceAtual];
+      imgElemento.style.opacity = 1;
+      atualizarPontos();
+    }, 500);
+  }
+
+  setInterval(trocarImagem, 4000);
+  atualizarPontos(); // Inicializa os pontos
+
+  // Se quiser permitir clique manual nos pontos:
+  pontos.forEach((ponto, index) => {
+    ponto.addEventListener('click', () => {
+      indiceAtual = index;
+      imgElemento.src = imagens[indiceAtual];
+      atualizarPontos();
+    });
+  });
